2e8e786ea5beca5b2c18fa947434e4caeb0d9dc5
Samaria Smothers <ssmothers457@noscihigh.org>
2021-02-24 18:48:24 +0000

    samaria done

--- index.js
+++ index.js
@@ -4,17 +4,29 @@
  * Using if, else-if and else, AND depending on the hour of the day, 
  * write a greeting program that prints a greeting to the screen like so: 
  *
+ * */
  * // hour is 0-11
  * Good Morning!
- *
+ **if(hours >=0 && hours<=11 ){
+   console.log("Good Morning.")
+ }
+ 
  * // hour is 12-16
  * Good Afternoon! 
- *
+ *if(hours >=12 && hours <=16){
+console.log("Good Afternoon ")
+ }
  * // hour is 17-21
  * Good Evening!
- *
+ *if(hours >=17 && hours <=21 ){
+   consolelog("Good Evening")
+ }
  * // hour is 22-24 
  * Good Night!
+ *if(hours >=22 && hours <=24){
+   console.log("Good Night ")
+ }
+ * 
  *
  * TIPS: 
  * 
